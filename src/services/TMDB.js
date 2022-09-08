92cc930d73935f7aa51033721e06256461c8ba3e
Frederick Bruce <frederick.bruce.dev@gmail.com>
2022-09-07 21:54:36 -0400

    Movie Information Update

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -17,7 +17,7 @@ export const tmdbApi = createApi({
     getMovies: builder.query({
       query: ({ genreIdOrCategoryName, page, searchQuery }) => {
         //* Get Movies by Search
-        if(searchQuery) {
+        if (searchQuery) {
           return `/search/movie?query=${searchQuery}&page={$page}&api_key=${tmdbApiKey}`;
         }
         //* Get Movies by Category
@@ -34,10 +34,16 @@ export const tmdbApi = createApi({
         return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
       },
     }),
+
+    // Get Movie
+    getMovie: builder.query({
+      query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
+    }),
   }),
 });
 
 export const {
   useGetMoviesQuery,
   useGetGenresQuery,
+  useGetMovieQuery,
 } = tmdbApi;
